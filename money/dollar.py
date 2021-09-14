e470ed2aa5329fee7e6de00938be92851f022b03
Simone Gaiarin <simgunz@gmail.com>
2021-09-14 17:38:17 +0200

    return a new dollar with the correct amount, green again

--- money/dollar.py
+++ money/dollar.py
@@ -3,5 +3,4 @@ class Dollar:
         self.amount = amount
 
     def times(self, multiplier):
-        self.amount *= multiplier
-        return Dollar(0)
+        return Dollar(self.amount * multiplier)

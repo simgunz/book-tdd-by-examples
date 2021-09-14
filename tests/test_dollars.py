3f27a45c01eb23c345d99f8d3ceb41dbbe85caf1
Simone Gaiarin <simgunz@gmail.com>
2021-09-14 17:35:20 +0200

    we want a dollar to represent a dollar and remove side effects

--- tests/test_dollars.py
+++ tests/test_dollars.py
@@ -2,8 +2,11 @@ import unittest
 
 from money.dollar import Dollar
 
+
 class TestDollar(unittest.TestCase):
     def test_multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
+        product = five.times(3)
+        self.assertEqual(15, product.amount)

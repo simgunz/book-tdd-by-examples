7b050b1ec32ead9af9a80022040053a1d642aa90
Simone Gaiarin <simgunz@gmail.com>
2021-09-15 17:52:09 +0200

    refactor test to use the new equality method we defined, test is green

--- tests/test_dollars.py
+++ tests/test_dollars.py
@@ -6,10 +6,8 @@ from money.dollar import Dollar
 class TestDollar(unittest.TestCase):
     def test_multiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+        self.assertEqual(Dollar(10), five.times(2))
+        self.assertEqual(Dollar(15), five.times(3))
 
     def test_equality(self):
         self.assertEqual(Dollar(5), Dollar(5))

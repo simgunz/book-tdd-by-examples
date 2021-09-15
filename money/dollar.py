aa95ef3d3dc37590e0c38f7021c4f24080534f4c
Simone Gaiarin <simgunz@gmail.com>
2021-09-15 18:01:14 +0200

    make it compile, we used a brutal copy paste to complete this step
    in this case we got the make the test green for free in one step

--- money/dollar.py
+++ money/dollar.py
@@ -7,3 +7,14 @@ class Dollar:
 
     def __eq__(self, dollar):
         return self._amount == dollar._amount
+
+
+class Franc:
+    def __init__(self, amount):
+        self._amount = amount
+
+    def times(self, multiplier):
+        return Franc(self._amount * multiplier)
+
+    def __eq__(self, dollar):
+        return self._amount == dollar._amount

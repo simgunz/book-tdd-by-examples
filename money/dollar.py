7f2b3b8b150b60b6af6114c9fa49f5fb23dec1e2
Simone Gaiarin <simgunz@gmail.com>
2021-10-01 17:59:21 +0200

    make eq common

--- money/dollar.py
+++ money/dollar.py
@@ -2,18 +2,15 @@ class Money:
     def __init__(self, amount):
         self._amount = amount
 
+    def __eq__(self, dollar):
+        return self._amount == dollar._amount
+
 
 class Dollar(Money):
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
 
-    def __eq__(self, dollar):
-        return self._amount == dollar._amount
-
 
 class Franc(Money):
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
-
-    def __eq__(self, dollar):
-        return self._amount == dollar._amount

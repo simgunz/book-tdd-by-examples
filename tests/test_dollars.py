cdc4f1bac4d6c1d9ec824b8fcb380c19135275a9
Simone Gaiarin <simgunz@gmail.com>
2021-10-01 18:02:17 +0200

    add missing test for franc equality before refactoring

--- tests/test_dollars.py
+++ tests/test_dollars.py
@@ -19,3 +19,7 @@ class TestFranc(unittest.TestCase):
         five = Franc(5)
         self.assertEqual(Franc(10), five.times(2))
         self.assertEqual(Franc(15), five.times(3))
+
+    def test_equality(self):
+        self.assertEqual(Franc(5), Franc(5))
+        self.assertNotEqual(Franc(5), Franc(6))

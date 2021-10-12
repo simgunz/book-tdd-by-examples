368a619d9733534e797e9d82df3eb949cb149b88
Simone Gaiarin <simgunz@gmail.com>
2021-10-12 18:03:53 +0200

    aggiunto test per metodo factory franc, test rossi

--- tests/test_currencies.py
+++ tests/test_currencies.py
@@ -1,6 +1,6 @@
 import unittest
 
-from money.currencies import Money, Franc
+from money.currencies import Money
 
 
 class TestDollar(unittest.TestCase):
@@ -12,15 +12,15 @@ class TestDollar(unittest.TestCase):
     def test_equality(self):
         self.assertEqual(Money.dollar(5), Money.dollar(5))
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))
-        self.assertNotEqual(Money.dollar(5), Franc(5))
+        self.assertNotEqual(Money.dollar(5), Money.franc(5))
 
 
 class TestFranc(unittest.TestCase):
     def test_multiplication(self):
-        five = Franc(5)
-        self.assertEqual(Franc(10), five.times(2))
-        self.assertEqual(Franc(15), five.times(3))
+        five = Money.franc(5)
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
 
     def test_equality(self):
-        self.assertEqual(Franc(5), Franc(5))
-        self.assertNotEqual(Franc(5), Franc(6))
+        self.assertEqual(Money.franc(5), Money.franc(5))
+        self.assertNotEqual(Money.franc(5), Money.franc(6))

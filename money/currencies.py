e88bd8c88a2458e6abf4cd182c601f07b2ef3b62
Simone Gaiarin <simgunz@gmail.com>
2021-10-12 17:57:46 +0200

    add simple static method for dollars, tests green

--- money/currencies.py
+++ money/currencies.py
@@ -2,6 +2,10 @@ class Money:
     def __init__(self, amount):
         self._amount = amount
 
+    @staticmethod
+    def dollar(amount):
+        return Dollar(amount)
+
     def __eq__(self, dollar):
         return self._amount == dollar._amount and isinstance(self, dollar.__class__)
 

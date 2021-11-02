82fe0e7af367a3a4f86262b677418816163bf39b
Simone Gaiarin <simgunz@gmail.com>
2021-11-02 17:41:09 +0100

    Add abstract method currency in Money, and implement it in dollar and franc

--- money/currencies.py
+++ money/currencies.py
@@ -1,4 +1,7 @@
-class Money:
+from abc import ABC, abstractmethod
+
+
+class Money(ABC):
     def __init__(self, amount):
         self._amount = amount
 
@@ -13,12 +16,22 @@ class Money:
     def __eq__(self, dollar):
         return self._amount == dollar._amount and isinstance(self, dollar.__class__)
 
+    @abstractmethod
+    def currency(self):
+        pass
+
 
 class Dollar(Money):
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
 
+    def currency(self):
+        return "USD"
+
 
 class Franc(Money):
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
+
+    def currency(self):
+        return "CHF"

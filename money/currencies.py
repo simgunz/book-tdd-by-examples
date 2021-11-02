e4262616448a612ac0d65393149cf2a1bce64b43
Simone Gaiarin <simgunz@gmail.com>
2021-11-02 18:07:38 +0100

    Refactor: use factory method in times, minor interruption
    I have interrupted what I was doing to clean up a method

--- money/currencies.py
+++ money/currencies.py
@@ -35,4 +35,4 @@ class Franc(Money):
         self._currency = "CHF"
 
     def times(self, multiplier):
-        return Franc(self._amount * multiplier)
+        return Money.franc(self._amount * multiplier)

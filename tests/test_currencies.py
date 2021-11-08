0f57277c5f63a6ebdee4d9790cce343d0b45131c
Simone Gaiarin <simgunz@gmail.com>
2021-11-08 18:17:16 +0100

    remove redundant equality tests

--- tests/test_currencies.py
+++ tests/test_currencies.py
@@ -29,7 +29,3 @@ class TestFranc(unittest.TestCase):
         five = Money.franc(5)
         self.assertEqual(Money.franc(10), five.times(2))
         self.assertEqual(Money.franc(15), five.times(3))
-
-    def test_equality(self):
-        self.assertEqual(Money.franc(5), Money.franc(5))
-        self.assertNotEqual(Money.franc(5), Money.franc(6))

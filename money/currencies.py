a79c6c260e16fa3585b6dea91b6e0eff7dc1f442
Simone Gaiarin <simgunz@gmail.com>
2021-11-08 18:03:21 +0100

    revert the change that returned money to go green before writing a new test

--- money/currencies.py
+++ money/currencies.py
@@ -30,9 +30,9 @@ class Money:
 
 class Dollar(Money):
     def times(self, multiplier):
-        return Money(self._amount * multiplier, self._currency)
+        return Dollar(self._amount * multiplier, self._currency)
 
 
 class Franc(Money):
     def times(self, multiplier):
-        return Money(self._amount * multiplier, self._currency)
+        return Franc(self._amount * multiplier, self._currency)

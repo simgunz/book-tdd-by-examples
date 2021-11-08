1d43a764f2c0ede32af8a5047d009303320cf80f
Simone Gaiarin <simgunz@gmail.com>
2021-11-08 18:19:04 +0100

    removed redundant tests and made single test class

--- tests/test_currencies.py
+++ tests/test_currencies.py
@@ -8,8 +8,6 @@ class TestMoney(unittest.TestCase):
         self.assertEqual("USD", Money.dollar(1).currency())
         self.assertEqual("CHF", Money.franc(1).currency())
 
-
-class TestDollar(unittest.TestCase):
     def test_multiplication(self):
         five = Money.dollar(5)
         self.assertEqual(Money.dollar(10), five.times(2))
@@ -19,10 +17,3 @@ class TestDollar(unittest.TestCase):
         self.assertEqual(Money.dollar(5), Money.dollar(5))
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))
         self.assertNotEqual(Money.dollar(5), Money.franc(5))
-
-
-class TestFranc(unittest.TestCase):
-    def test_multiplication(self):
-        five = Money.franc(5)
-        self.assertEqual(Money.franc(10), five.times(2))
-        self.assertEqual(Money.franc(15), five.times(3))

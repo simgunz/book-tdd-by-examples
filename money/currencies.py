d113fc009025c7d855cc540ed4218dcfb5246757
Simone Gaiarin <simgunz@gmail.com>
2021-11-08 18:08:46 +0100

    return money from franc times

--- money/currencies.py
+++ money/currencies.py
@@ -30,7 +30,7 @@ class Money:
 
 class Dollar(Money):
     def times(self, multiplier):
-        return Dollar(self._amount * multiplier, self._currency)
+        return Money(self._amount * multiplier, self._currency)
 
 
 class Franc(Money):

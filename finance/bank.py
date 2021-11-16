c9f78b38ab1ff8d9dd5dced77d8f04bb8fda4c64
Simone Gaiarin <simgunz@gmail.com>
2021-11-16 18:36:20 +0100

    well I did a mistake, but the test was still green, fixed now

--- finance/bank.py
+++ finance/bank.py
@@ -4,4 +4,4 @@ from finance.currencies import Money
 class Bank:
     def reduce(self, source, to):
         amount = source.augend.amount + source.addend.amount
-        return Money.dollar(amount)
+        return Money(amount, to)

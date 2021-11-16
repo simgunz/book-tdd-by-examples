031a0b643dc4700f942f372c686a2b904dd11379
Simone Gaiarin <simgunz@gmail.com>
2021-11-16 17:31:25 +0100

    refactor: make amount a public property

--- currencies.py
+++ currencies.py
@@ -1,6 +1,6 @@
 class Money:
     def __init__(self, amount, currency):
-        self._amount = amount
+        self.amount = amount
         self._currency = currency
 
     @staticmethod
@@ -12,13 +12,13 @@ class Money:
         return Money(amount, "CHF")
 
     def __repr__(self):
-        return "Money(%s, %s)" % (self._amount, self._currency)
+        return "Money(%s, %s)" % (self.amount, self._currency)
 
     def __eq__(self, other):
-        return self._currency == other.currency() and self._amount == other._amount
+        return self._currency == other.currency() and self.amount == other.amount
 
     def times(self, multiplier):
-        return Money(self._amount * multiplier, self._currency)
+        return Money(self.amount * multiplier, self._currency)
 
     def currency(self):
         return self._currency

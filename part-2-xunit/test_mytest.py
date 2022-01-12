df96565bda5ea7a11afc8d39fe87b136e7f65d4c
Simone Gaiarin <simgunz@gmail.com>
2022-01-12 09:37:50 +0100

    add failing test for a test summary

--- part-2-xunit/test_mytest.py
+++ part-2-xunit/test_mytest.py
@@ -7,5 +7,11 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert "setUp testMethod tearDown " == self.test.log
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert "1 run, 0 failed" == result.summary()
+
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()

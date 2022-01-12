566e32ab32acc92615b5c766f52248a1b8f5348f
Simone Gaiarin <simgunz@gmail.com>
2022-01-12 09:45:51 +0100

    add a property to count the number of times a method is run

--- part-2-xunit/mytest.py
+++ part-2-xunit/mytest.py
@@ -34,5 +34,8 @@ class WasRun(TestCase):
 
 
 class TestResult:
+    def __init__(self):
+        self.runCount = 1
+
     def summary(self):
-        return "1 run, 0 failed"
+        return f"{self.runCount} run, 0 failed"

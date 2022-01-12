7e175adf7db710073eb7e9f80520a3e4d8589c2e
Simone Gaiarin <simgunz@gmail.com>
2022-01-12 09:18:40 +0100

    remove redundant (?) assert

--- part-2-xunit/test_mytest.py
+++ part-2-xunit/test_mytest.py
@@ -4,7 +4,6 @@ from mytest import TestCase, WasRun
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        assert not test.wasRun
         test.run()
         assert test.wasRun
 

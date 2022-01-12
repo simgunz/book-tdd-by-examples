a08da51ee70351d58b16ece560dc6dadab2b2c62
Simone Gaiarin <simgunz@gmail.com>
2022-01-12 09:56:26 +0100

    record errors in TestResult

--- part-2-xunit/mytest.py
+++ part-2-xunit/mytest.py
@@ -44,9 +44,13 @@ class WasRun(TestCase):
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount += 1
+
+    def testFailed(self):
+        self.errorCount += 1
 
     def summary(self):
         return f"{self.runCount} run, 0 failed"

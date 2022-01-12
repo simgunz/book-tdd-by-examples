e393c249f13a078503dacf9f5a28ff194c15f4a2
Simone Gaiarin <simgunz@gmail.com>
2022-01-12 09:29:42 +0100

    remove setUp in the tests because not necessary anymore

--- part-2-xunit/test_mytest.py
+++ part-2-xunit/test_mytest.py
@@ -2,10 +2,8 @@ from mytest import TestCase, WasRun
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
         self.test.run()
         assert "setUp testMethod " == self.test.log
 

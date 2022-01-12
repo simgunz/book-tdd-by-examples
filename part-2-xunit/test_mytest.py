5ad3db4905ad444a8e0d6a62a749c8720816cd89
Simone Gaiarin <simgunz@gmail.com>
2022-01-12 09:29:20 +0100

    remove redundant testRunning test

--- part-2-xunit/test_mytest.py
+++ part-2-xunit/test_mytest.py
@@ -5,14 +5,9 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert self.test.wasRun
-
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert "setUp testMethod " == self.test.log
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()

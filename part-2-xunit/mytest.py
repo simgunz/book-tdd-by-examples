1b18c79026fdcbcc1fc3602f09d5e0ec7a742f7d
Simone Gaiarin <simgunz@gmail.com>
2022-01-13 10:45:18 +0100

    add simple TestSuite run method, test red

--- part-2-xunit/mytest.py
+++ part-2-xunit/mytest.py
@@ -65,3 +65,9 @@ class TestSuite:
 
     def add(self, test):
         self.tests.append(test)
+
+    def run(self):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+        return result
